fae35a13a3e34675e6e1ddcecbc795dd5fa05016
nonzinv <nonzinv@uw.edu>
2024-06-03 12:26:00 -0700

    changes

--- Views/ShoppingListView.swift
+++ Views/ShoppingListView.swift
@@ -11,7 +11,6 @@ import FirebaseCore
 struct ShoppingListView: View {
     @EnvironmentObject var viewModel: GroceryViewModel
     @State private var newItem: String = ""
-    @State private var showingAddItemSheet = false
 
     var body: some View {
         NavigationView {
@@ -21,63 +20,25 @@ struct ShoppingListView: View {
                     .modifier(TitleModifier())
                 
                 List {
+                    HStack {
+                        TextField("Enter new item", text: $newItem, onCommit: addItem)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                        Button(action: addItem) {
+                            Image(systemName: "plus.circle.fill")
+                                .foregroundColor(.blue)
+                                .font(.title)
+                        }
+                    }
+                    .padding()
+
                     ForEach(viewModel.shoppingList) { item in
                         Text(item.name)
                     }
                     .onDelete(perform: deleteItems)
                 }
-                .navigationBarItems(trailing: Button(action: {
-                    showingAddItemSheet = true
-                }) {
-                    Image(systemName: "plus")
-                })
-            }
-            .sheet(isPresented: $showingAddItemSheet) {
-                AddItemView(showingSheet: $showingAddItemSheet, viewModel: viewModel)
+                .navigationBarTitle("Shopping List")
             }
         }
-    }
-
-    private func deleteItems(at offsets: IndexSet) {
-        viewModel.deleteShoppingItem(at: offsets)
-    }
-}
-
-struct AddItemView: View {
-    @Binding var showingSheet: Bool
-    @EnvironmentObject var viewModel: GroceryViewModel
-    @State private var newItem: String = ""
-
-    var body: some View {
-        VStack {
-            Text("Add New Item")
-                .font(.headline)
-                .padding()
-            
-            TextField("Enter new item", text: $newItem)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-            
-            HStack {
-                Button("Add") {
-                    addItem()
-                }
-                .padding()
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .cornerRadius(8)
-                
-                Button("Cancel") {
-                    showingSheet = false
-                }
-                .padding()
-                .background(Color.gray)
-                .foregroundColor(.white)
-                .cornerRadius(8)
-            }
-            .padding()
-        }
-        .padding()
     }
 
     private func addItem() {
@@ -85,6 +46,9 @@ struct AddItemView: View {
         let newShoppingItem = ShoppingListItem(id: UUID().uuidString, name: newItem)
         viewModel.addShoppingItem(newShoppingItem)
         newItem = ""
-        showingSheet = false
+    }
+
+    private func deleteItems(at offsets: IndexSet) {
+        viewModel.deleteShoppingItem(at: offsets)
     }
 }
